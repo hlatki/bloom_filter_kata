4267a87256edc93eec7126d691a39552819b0ecc
hlatki <hannahatkinson@gmail.com>
2013-09-25 23:34:22 -0400

    Add standard dev and do some cleanup

--- bloom.cpp
+++ bloom.cpp
@@ -5,27 +5,50 @@
 #include <iostream>
 #include <bitset>
 #include <set>
+#include <map>
+#include <cmath>
 
+
+#define BUCKETS 2358870
 
 using namespace std;
 
+void printHashStats(map<int,int> hashes) {
+    map<int, int>::iterator it;
+    int sum = 0;
+    float avg = 0.0; 
+    long diffSum = 0;
+    float diffAvg = 0;
+    float standardDev = 0;
+
+
+    for(map<int, int>::iterator it = hashes.begin(); it != hashes.end(); ++it) {
+        sum += it->second;
+    }
+    avg = (float) sum / (float) hashes.size();
+
+    for(map<int, int>::iterator it = hashes.begin(); it != hashes.end(); ++it) {
+       diffSum += pow((double)(avg - it->second), 2.0); 
+    }
+    diffAvg = (float) diffSum / (float) hashes.size();
+    standardDev = sqrt(diffAvg);
+
+
+    cout << "\tAverage bucket contains: " << avg;
+    cout << "\n\tWith standard deviation: " << standardDev;
+}
 
 int main() {
 
-    bitset<2358870> lookup;
+    bitset<BUCKETS> lookup;
 
     for (int i = 0; i < lookup.size(); i++){
         lookup.set(i,0);
     }
+
+
+    
     char dictionaryPath[] = "/usr/share/dict/words";
-    //char t[] = "cat";
-    /*string t = "cat";
-    int t_hashed_murmur = MurmurHash2(t.c_str(), t.length(), 0);
-    long t_hashed_djb2 = djb2(t.data(), t.length());
-    printf("hashed value (murmur) is: %d\n", t_hashed_murmur);
-    printf("hashed value (djb2) is: %lu\n", t_hashed_djb2);
-    printf("shortened value (murmur) is: %d\n", shorten((long)t_hashed_murmur));
-    printf("shortened value (djb2) is: %d\n\n", shorten(t_hashed_djb2));*/
    
     set<long> wordHashes;
 
@@ -33,8 +56,11 @@ int main() {
     dictionary.open(dictionaryPath);
     int collCount = 0;
     string line;
-    int boxes = 2358870;
+    int buckets = BUCKETS;
     int wordCount = 0;
+
+    map<int, int> murmurMap;
+    map<int, int> djb2Map;
     while(!dictionary.eof()) {
         getline(dictionary, line);
         wordCount++;
@@ -43,31 +69,39 @@ int main() {
         long b;
         //a = shorten(MurmurHash2(line.c_str(), line.length(), 0));
         //a = shorten(djb2(line.c_str(), line.length()));
-        a = MurmurHash2(line.c_str(), line.length(), 0) % boxes;
-        b = djb2(line.c_str(), line.length()) % boxes;
+        a = MurmurHash2(line.c_str(), line.length(), 0) % buckets;
+        b = djb2(line.c_str(), line.length()) % buckets;
         lookup[a] = 1;
         lookup[b] = 1;
         wordHashes.insert(a);
         wordHashes.insert(b);
+
+        murmurMap[a] += 1;
+        djb2Map[b] += 1;
         if (a == b) {
             collCount++;
             cout << line << "\n";
         }
-
-
-
     }
-    
+    cout << "Bucket Size: " << BUCKETS << "\n";
     printf("There were %d collisions.\n",collCount);
     printf("%d words in dictionary.\n",wordCount);
     cout << wordHashes.size() << " items in set of hashes.\n";
+    cout << "\n\n";
+    cout << "Hash distribution: \n";
+    cout << "For Murmur: \n"; 
+    printHashStats(murmurMap);
+    cout << "\n";
+    cout << "For djb2: \n"; 
+    printHashStats(djb2Map);
+    cout << "\n";
 
     while(1) {
         string input;
         cout << "Enter string to test: ";
         getline(cin,input);
-        int t1 =  MurmurHash2(input.c_str(), input.length(), 0) % boxes;
-        int t2 =  djb2(input.c_str(), input.length()) % boxes;
+        int t1 =  MurmurHash2(input.c_str(), input.length(), 0) % buckets;
+        int t2 =  djb2(input.c_str(), input.length()) % buckets;
 
         cout << "t1 is: " << lookup[t1] << " & t2 is " << lookup[t2] << "\n";
         if (lookup[t1] && lookup[t2]) {
